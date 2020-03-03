0916f372c68e2c0cb0d21e55be67b7bc7d27de60
angelneychev <angel.neychev@gmail.com>
2020-03-03 15:50:39 +0200

    Add DataModel ClubRegisterNumber.cs

---
+++ Data/Dalmatian.Data.Models/ClubRegisterNumber.cs
@@ -0,0 +1,9 @@
+﻿using Dalmatian.Data.Common.Models;
+
+namespace Dalmatian.Data.Models
+{
+    public class ClubRegisterNumber : BaseDeletableModel<int>
+    {
+        public string ClubNumber { get; set; }
+    }
+}
--- Data/Dalmatian.Data/ApplicationDbContext.cs
+++ Data/Dalmatian.Data/ApplicationDbContext.cs
@@ -26,6 +26,8 @@
 
         public DbSet<Setting> Settings { get; set; }
 
+        public DbSet<ClubRegisterNumber> ClubRegisterNumbers { get; set; }  
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
