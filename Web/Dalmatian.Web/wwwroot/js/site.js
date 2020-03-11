ee931fd72f6c0f86a0a6ef382c2d6b6a80d79264
Angel Neychev <angel.neychev@gmail.com>
2020-03-11 19:05:00 +0200

    Update ByDogName.cshtml

--- Web/Dalmatian.Web/wwwroot/js/site.js
+++ Web/Dalmatian.Web/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+img.onclick = function(){
+        modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+                modal.style.display = "none";
+          }
