9d8501ab9bbefd5ee895c896ab2cd85722f2aa85
Angel Neychev <angel.neychev@gmail.com>
2020-03-27 21:52:47 +0200

    add RegistrationKennelNumber.cs

---
+++ Data/Dalmatian.Data.Models/RegistrationKennelNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace Dalmatian.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Dalmatian.Data.Common.Models;
+
+    public class RegistrationKennelNumber : BaseDeletableModel<int>
+    {
+        public string RegistrationNumber { get; set; }
+
+        public int DogId { get; set; }
+
+        public Dog Dog { get; set; }
+
+        public DateTime DateOfRegister { get; set; }
+    }
+}
